e8cd7b501de15acd1195bc4e54e7ab79fb6b9e4b
Alonso Rojas <bigshot.developer928@gmail.com>
2018-12-30 11:42:05 -0400

    portal tjmg automation release

---
+++ README.md
@@ -0,0 +1,62 @@
+# portal tjmg automation
+
+## Installation
+Use python3.6
+
+## Run script via terminal
+-number - list of search numbers
+
+-csv_numbers - csv file with numbers
+
+-csv_words - csv file with words
+
+-download_folder - a place, where download pdf files if the folder doesn't exists will be created new if parameter not set, will save in a current folder
+
+#### With csv file
+```
+python3 tjmg_automation.py -csv_numbers number.csv -csv_words words.csv -download_folder ./files/
+```
+1. Read numbers from the number.csv
+2. Read numbers from the words.csv
+3. Download pdf files in  ./files/ can be set absolute path as example(/home/ubuntu/intel_bot_sentenca_tjmg/)
+
+#### With number list
+```
+python3 tjmg_automation.py -number 2008.001.184665-8 0156353-61.2014.8.19.0038 -csv_words words.csv -download_folder ./files2/
+```
+1. Read numbers from the command line number split by space num1 num2 num3
+2. Read numbers from the words.csv
+3. Download pdf files in  ./files2/ will be created if doesn't exists
+
+# How to use python module
+
+#### With csv file
+csv_parsing() - function save pdf files in work_folder.
+```
+from tjmg_automation import TjmgAutomation
+search_words = ["Ver íntegra do(a) Sentença", "Tipo do Movimento"]
+ja = TjmgAutomation(headless=True)
+ja.csv_parsing(csv_file='number.csv',search_words, work_folder='./files/')
+```
+#### With number
+search_process() - save pdf in work_folder and return text of pdf
+```
+from tjmg_automation import TjmgAutomation
+
+search_words = ["Ver íntegra do(a) Sentença", "Tipo do Movimento"]
+ja = TjmgAutomation(headless=True)
+ja.search_process(number='2008.001.184665-8', search_word=search_words, work_folder='./')
+ja.search_process(number='0156353-61.2014.8.19.0038', search_word=search_words, work_folder='./')
+```
+# How to kill process
+press CTRL + C
+
+if not help
+```
+ps -aux | grep 'tjmg_automation.py'
+ubuntu   16875  1.3  0.7 104408 31584 pts/1    S+   20:36   0:11 python3 tjmg_automation.py -csv_file number.csv -download_folder ./files/
+```
+id process 16875
+```
+kill 16875
+```
---
+++ tjmg_automation.py
@@ -0,0 +1,299 @@
+# -*- coding: UTF-8 -*-
+import platform
+import argparse
+import csv
+import time
+from enum import Enum
+from io import BytesIO
+import sys
+import os
+import logging
+import re
+
+import requests
+import pdfkit
+from selenium import webdriver
+from selenium.common.exceptions import (
+    NoSuchElementException,
+    UnexpectedAlertPresentException
+)
+from selenium.common.exceptions import TimeoutException
+from PIL import Image
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_URL = 'http://www.tjmg.jus.br/portal-tjmg/'
+URL_POST_CAPTCHA = 'http://2captcha.com/in.php'
+URL_GET_CAPTCHA = 'http://2captcha.com/res.php'
+KEY = 'c3b78102059c7d2009ea1591019068c6'
+COUNT_NUMBERS = 0
+CURRENT_NUMBERS = 0
+logging.basicConfig(
+    filename='scanning.log',
+    filemode='w',
+    format='%(name)s - %(levelname)s - %(message)s'
+)
+
+if platform.system() == "Windows":
+    CHROME_DRIVER_PATH = os.path.join(BASE_DIR, "chrome", "windows", "chromedriver.exe")
+elif platform.system() == "Linux":
+    CHROME_DRIVER_PATH = os.path.join(BASE_DIR, "chrome", "linux", "chromedriver")
+else:
+    CHROME_DRIVER_PATH = os.path.join(BASE_DIR, "chrome", "mac", "chromedriver")
+
+
+class TjmgAutomation(object):
+    def __init__(self, download_folder, headless=False):
+        self.headless = headless
+        self.driver = self.session()
+        self.download_folder = download_folder
+        self.enable_download_in_headless_chrome()
+
+    def enable_download_in_headless_chrome(self):
+        # add missing support for chrome "send_command"  to selenium webdriver
+        self.driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
+
+        params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': self.download_folder}}
+        command_result = self.driver.execute("send_command", params)
+
+    def session(self):
+        chrome_options = webdriver.ChromeOptions()
+        if self.headless is True:
+            chrome_options.add_argument('-headless')
+        # PROXY = "142.93.87.88:3128"
+        # chrome_options.add_argument('--proxy-server=%s' % PROXY)
+        self.driver = webdriver.Chrome(CHROME_DRIVER_PATH, chrome_options=chrome_options)
+
+        self.driver.set_page_load_timeout(50)
+        self.driver.set_window_size(1360, 900)
+
+        return self.driver
+
+    def rename(self, file_name, number, word):
+        os.rename(self.download_folder + '/' + file_name,
+                  self.download_folder + '/' + number + '_' + word + '.pdf')
+
+    def search_process(self, number, search_word=None, work_folder='./'):
+        try:
+            self.driver.get(BASE_URL)
+        except TimeoutException:
+            logging.warning(
+                    '{} - Timeout in loading website'.format(
+                        number
+                    )
+                )
+            return None
+
+        try:
+            self.driver.find_elements_by_xpath("//section[@class='tabs-of-process']"
+                                               "//input[@id='txtProcesso']")[0].send_keys(number)
+            self.driver.find_elements_by_xpath("//section[@class='tabs-of-process']"
+                                               "//form[@class='first-instance-form']"
+                                               "//button[@type='submit']")[0].click()
+        except NoSuchElementException:
+            logging.warning(
+                '{} - Element not found.'.format(
+                    number
+                )
+            )
+            return None
+
+        captcha_pass = False
+        for i in range(3):
+            if self.resolve_captcha() is True:
+                captcha_pass = True
+                break
+
+        if not captcha_pass:
+            logging.warning(
+                '{} - Captcha failed.'.format(
+                    number
+                )
+            )
+            return None
+
+        try:
+            self.driver.find_elements_by_xpath("//*[contains(text(), ' Andamentos')]")[0].click()
+
+        except NoSuchElementException:
+            logging.warning(
+                '{} - Element not found.'.format(
+                    number
+                )
+            )
+            return None
+
+        all_tr_items = self.driver.find_elements_by_xpath("//table[@class='corpo']/tbody/tr[contains(@class, 'linha')]")
+
+        try:
+            for word in search_word:
+                for tr in all_tr_items:
+                    td_elems = tr.find_elements_by_xpath("td")
+                    if word in td_elems[1].text.strip():
+                        download_btn = td_elems[0].find_elements_by_xpath(".//a")[0]
+                        download_btn.click()
+                        doc_id = download_btn.get_attribute("href")
+                        doc_id = re.search(r"javascript:mostrarOcultarPanel\((.*)?\)", doc_id).group(1)[1:][:-1]
+                        download_btn = self.driver.find_elements_by_xpath("//table[@id='painelMov" + doc_id + "']//a")[0]
+                        pdf_url_name = download_btn.text
+                        download_btn.click()
+                        time.sleep(5)
+                        self.rename(pdf_url_name, number, td_elems[1].text.strip())
+        except NoSuchElementException:
+            logging.warning(
+                '{} - Download failed.'.format(
+                    number
+                )
+            )
+            return None
+
+        return True
+
+    def resolve_captcha(self):
+        try:
+            self.driver.find_element_by_id('captcha_image')
+        except NoSuchElementException:
+            return True
+
+        try:
+            size_image = 1360, 900
+            element = self.driver.find_element_by_id('captcha_image')
+            location = element.location
+            size = element.size
+            png = self.driver.get_screenshot_as_png()
+            im = Image.open(BytesIO(png))
+            im.thumbnail(size_image, Image.ANTIALIAS)
+            left = location['x']
+            top = location['y']
+            right = location['x'] + size['width']
+            bottom = location['y'] + size['height']
+            im = im.crop((left, top, right, bottom))
+            im.save('screenshot.png')
+
+            files = {'file': open('screenshot.png', 'rb')}
+            data = {'key': KEY}
+            response = requests.post(
+                URL_POST_CAPTCHA,
+                files=files,
+                data=data,
+                timeout=15
+            )
+            if response.ok:
+                time.sleep(15)
+                id_message = response.text.split('|')[-1]
+                resolved_captcha = requests.get(
+                    '{}?key={}&action=get&id={}'.format(
+                        URL_GET_CAPTCHA,
+                        KEY,
+                        id_message
+                    ),
+                    timeout=15
+                )
+                message = resolved_captcha.text.split('|')[-1]
+                self.driver.find_element_by_id('captcha_text').send_keys(message)
+                return True
+        except:
+            return False
+
+    def csv_parsing(self, csv_file, csv_words, work_folder='./'):
+        global COUNT_NUMBERS
+
+        with open(csv_file, newline='') as f1:
+            reader1 = csv.DictReader(f1)
+            COUNT_NUMBERS = len([_ for _ in reader1])
+
+        with open(csv_file, newline='') as f2:
+            reader2 = csv.DictReader(f2)
+            for row in reader2:
+                # try:
+                    self.search_process(
+                        number=row['processo'],
+                        search_word=csv_words,
+                        work_folder=work_folder
+                    )
+
+                    self.progress_bar()
+                # except Exception as e:
+                #     pass
+                    # logging.warning(
+                    #     '{} - Download do arquivo não permitido'.format(
+                    #         row['processo']
+                    #     )
+                    # )
+
+    @staticmethod
+    def progress_bar():
+        global COUNT_NUMBERS
+        global CURRENT_NUMBERS
+
+        CURRENT_NUMBERS += 1
+        sys.stdout.write("\r" + str(
+            'Progress Downloading Numbers: {}/{}'.format(
+                CURRENT_NUMBERS,
+                COUNT_NUMBERS
+            )
+        ))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='portal tjmg automation.')
+    parser.add_argument(
+        '-csv_numbers',
+        dest='csv_numbers',
+        type=str,
+        help='Input CSV numbers'
+    )
+    parser.add_argument(
+        '-csv_words',
+        dest='csv_words',
+        type=str,
+        help='Input CSV words'
+    )
+    parser.add_argument(
+        '-download_folder',
+        dest='download_folder',
+        type=str,
+        help='Folder where will be stored pdf files',
+        default='./'
+    )
+    parser.add_argument(
+        '-number',
+        dest='number',
+        type=str,
+        help='Number of search pdf',
+        action='store',
+        nargs='*',
+    )
+    args = parser.parse_args()
+
+    ja = TjmgAutomation(args.download_folder, headless=True)
+    search_words = []
+
+    if args.download_folder:
+        if not os.path.exists(args.download_folder):
+            os.makedirs(args.download_folder)
+
+    if args.csv_words:
+        with open(args.csv_words, newline='') as f:
+            search_words = [i.replace('\n', '') for i in f.readlines()]
+
+    if args.csv_numbers and args.csv_words:
+        ja.csv_parsing(
+            args.csv_numbers,
+            search_words,
+            work_folder=args.download_folder
+        )
+        ja.driver.quit()
+        sys.exit()
+
+    elif args.number and args.csv_words:
+        COUNT_NUMBERS = len(args.number)
+        for num in args.number:
+            ja.search_process(num, search_words, args.download_folder)
+            ja.progress_bar()
+
+    if args.download_folder:
+        sys.stdout.write(
+            "\nFiles stored in folder: {}\n".format(args.download_folder)
+        )
+
+    ja.driver.quit()
