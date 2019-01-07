ec25e7496cc71a51029f265debd18bbcc7bb99a5
Alonso Rojas <bigshot.developer928@gmail.com>
2019-01-07 10:47:28 -0400

    complete tjmg automation

--- tjmg_automation.py
+++ tjmg_automation.py
@@ -94,6 +94,7 @@ class TjmgAutomation(object):
             self.driver.find_elements_by_xpath("//section[@class='tabs-of-process']"
                                                "//form[@class='first-instance-form']"
                                                "//button[@type='submit']")[0].click()
+            time.sleep(2)
         except NoSuchElementException:
             logging.warning(
                 '{} - Webdriver Element not found.'.format(
@@ -131,7 +132,7 @@ class TjmgAutomation(object):
                 )
             )
             return None
-
+        time.sleep(3)
         try:
             self.driver.find_elements_by_xpath("//*[contains(text(), ' Andamentos')]")[0].click()
         except:
@@ -146,13 +147,17 @@ class TjmgAutomation(object):
         record_number = len(all_tr_items)
 
         all_files_downloaded = False
-        try:
-            for word in search_word:
-                file_downloaded = False
-                for i in range(0, record_number):
+
+        for word in search_word:
+            file_downloaded = False
+            for i in range(0, record_number):
+                try:
                     td_elems = self.driver.find_elements_by_xpath("//table[@class='corpo']/tbody/tr[contains(@class, 'linha')]")[i].find_elements_by_xpath("td")
                     item_name = td_elems[1].text.strip()
-                    if word in item_name:
+                except:
+                    continue
+                if word in item_name:
+                    try:
                         if len(td_elems[0].find_elements_by_xpath(".//a")) > 0:
                             download_btn = td_elems[0].find_elements_by_xpath(".//a")[0]
                         else:
@@ -166,7 +171,10 @@ class TjmgAutomation(object):
                             continue
                         file_name = download_btn.text
                         download_btn.click()
-                        time.sleep(3)
+                        time.sleep(2)
+                    except:
+                        continue
+                    try:
                         my_file = Path(self.download_folder + file_name)
                         if my_file.is_file():
                             self.rename(file_name, number, item_name)
@@ -177,25 +185,18 @@ class TjmgAutomation(object):
                             self.driver.execute_script("window.history.go(-1)")
                             time.sleep(1)
                             self.rename(file_name + '.pdf', number, item_name)
-                        file_downloaded = True
-                        all_files_downloaded = True
-                if not file_downloaded:
-                    logging.warning(
-                        '{} - {} - Arquivo não existe.'.format(
-                            number,
-                            word
-                        )
+                    except:
+                        continue
+                    file_downloaded = True
+                    all_files_downloaded = True
+            if not file_downloaded:
+                logging.warning(
+                    '{} - {} - Arquivo não existe.'.format(
+                        number,
+                        word
                     )
-            if not all_files_downloaded:
-                return None
-        except Exception as e:
-            logging.warning(
-                '{} - Arquivo não existe.'.format(
-                    number
                 )
-            )
-            if all_files_downloaded:
-                return True
+        if not all_files_downloaded:
             return None
 
         return True
